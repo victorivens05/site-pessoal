93afc21eb30a8764d863140071e917de1ab2cd40
Victor Ivens Pereira <victorivens05@gmail.com>
2020-12-16 07:17:57 -0300

    api

---
+++ packages/api/src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common'
+import { AppService } from './app.service'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello (): string {
+    return this.appService.getHello()
+  }
+  @Get('/api')
+  getHello2 () {
+    return { 'hello': 'world' }
+  }
+
+  @Get('/api/:id')
+  getHelloId (@Param('id') id) {
+    return { hello: 'world', 'id': id }
+  }
+}
