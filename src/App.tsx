53b1165524a7f26c65ca8e60deb2cb87c0d00809
Victor Ivens Pereira <victorivens05@gmail.com>
2021-02-11 10:25:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Site do vitão<br />
+          Site do vitão 2<br />
           url: {process.env.REACT_APP_API_URL}<br />
           host: {process.env.REACT_APP_API_HOST}<br />
         </p>
